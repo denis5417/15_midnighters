88aeedbb66df87b5247f9c19ca7870cbb5ec5f84
denis5417 <denisz.pol@yandex.ru>
2017-07-26 09:28:07 +0300

    Написан скрипт и README

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# Night Owls Detector
+# Детектор "Сов'
 
-[TODO. There will be project description]
+Скрипт получает информацию из [API DEVMAN](https://devman.org/api/challenges/solution_attempts/) и выводит пользователей, 
+отправлявших решения с полуночи до 5 утра
 
-# Project Goals
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+# Установка и использование
+
+Не требует установки дополнительных библиотек
+
+Использование:
+```#!bash
+$ python3 seek_dev_nighters.py 
+id240095620
+pinguine
+rs.porosenok
+maksim.artemyev
+id290437617
+piterskikhsa
+tonyflex
+id31656040
+KhorinVitaly
+```
+
+# Цели проекта
+
+Этот код написан в образовательных целях. Тренировочный курс для веб-девелоперов - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,31 @@
+import json
+import requests
+from datetime import datetime, time
+import pytz
+
+LINK = "https://devman.org/api/challenges/solution_attempts"
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    attempts_data = json.loads(requests.get(LINK).text)
+    pages_count = attempts_data['number_of_pages']
+    for page in range(1, pages_count + 1):
+        params = {"page": page}
+        attempts_data = json.loads(requests.get(LINK, params=params).text)
+        for attempt in attempts_data['records']:
+            if attempt['timestamp']:  # sometimes data is invalid
+                yield attempt
+
 
 def get_midnighters():
-    pass
+    right_tz = pytz.timezone('Europe/Moscow')
+    midnighters = []
+    for attempt in load_attempts():
+        attempt_date = pytz.timezone(attempt['timezone']).localize(datetime.fromtimestamp(attempt['timestamp']))
+        right_attempt_time = attempt_date.astimezone(right_tz).time()
+        if time(0, 0) <= right_attempt_time <= time(5, 0):
+            midnighters.append(attempt['username'])
+    return set(midnighters)  # usernames should not be repeated
 
 if __name__ == '__main__':
-  pass
+    print(*get_midnighters(), sep="\n")
