88bcaff14c7d5c23b27ced36d228bcd382153da3
denis5417 <denisz.pol@yandex.ru>
2017-07-28 07:54:17 +0300

    Update seek_dev_nighters.py

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -4,23 +4,30 @@ from datetime import datetime
 import pytz
 
 
-def load_attempts(link):
+def load_all_attempts(link):
     attempts_data = json.loads(requests.get(link).text)
     pages_count = attempts_data['number_of_pages']
-    if attempts_data['records'][0]['timestamp']:
-        yield attempts_data['records'][0]['timestamp']
+    attempts = load_attempts_from_page(attempts_data['records'])
+    for attempt in attempts:
+        yield attempt
     for page in range(2, pages_count+1):
         params = {"page": page}
         attempts_data = json.loads(requests.get(link, params=params).text)
-        for attempt in attempts_data['records']:
-            if attempt['timestamp']:  # sometimes data is invalid
-                yield attempt
+        attempts = load_attempts_from_page(attempts_data['records'])
+        for attempt in attempts:
+            yield attempt
+
+
+def load_attempts_from_page(records):
+    for attempt in records:
+        if attempt['timestamp']:  # sometimes data is invalid
+            yield attempt
 
 
 def get_midnighters():
     right_tz = pytz.timezone('Europe/Moscow')
     midnighters = []
-    for attempt in load_attempts("https://devman.org/api/challenges/solution_attempts"):
+    for attempt in load_all_attempts("https://devman.org/api/challenges/solution_attempts"):
         attempt_date = pytz.timezone(attempt['timezone']).localize(datetime.fromtimestamp(attempt['timestamp']))
         right_attempt_time = attempt_date.astimezone(right_tz).time()
         start_of_night = 0
